d3a9857573302738e46d01bf45bbbd83af7bd509
jinpu <jinpu@LAPTOP-J1SUAON8>
2018-10-13 20:18:50 +0900

    各種構造体追加

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,10 @@ use std::io::BufRead;
 use std::fs::File;
 use std::fs::OpenOptions;
 
+use usiagent::error::*;
+use usiagent::shogi::*;
+use usiagent::TryFrom;
+
 use error::*;
 
 pub trait CsaStream {
@@ -76,5 +80,95 @@ impl CsaStream for CsaFileStream {
 				Ok(Some(lines[p as usize].clone()))
 			}
 		}
+	}
+}
+#[derive(Debug)]
+pub struct CsaData {
+	pub version:Option<String>,
+	pub kifu_info:Option<KifuInfo>,
+	pub initial_position:Banmen,
+	pub initial_mochigoma:MochigomaCollections,
+	pub moves:Vec<Move>,
+	pub elapsed:Vec<u32>,
+	pub end_state:EndState,
+}
+impl CsaData {
+	pub fn new(banmen:Banmen,
+				mochigoma:MochigomaCollections,
+				mvs:Vec<Move>, elapsed:Vec<u32>, end_state:EndState) -> CsaData {
+		CsaData {
+			version:None,
+			kifu_info:None,
+			initial_position:banmen,
+			initial_mochigoma:mochigoma,
+			moves:mvs,
+			elapsed:elapsed,
+			end_state:end_state,
+		}
+	}
+}
+#[derive(Debug)]
+pub struct KifuInfo {
+	pub sente_name:Option<String>,
+	pub gote_name:Option<String>,
+	pub event:Option<String>,
+	pub site:Option<String>,
+	pub start_time:Option<String>,
+	pub end_time:Option<String>,
+	pub time_limit:Option<(u32,Option<u32>)>,
+	pub opening:Option<String>,
+}
+impl KifuInfo {
+	pub fn new() -> KifuInfo {
+		KifuInfo {
+			sente_name:None,
+			gote_name:None,
+			event:None,
+			site:None,
+			start_time:None,
+			end_time:None,
+			time_limit:None,
+			opening:None,
+		}
+	}
+}
+#[derive(Debug)]
+pub enum EndState {
+	Toryo, // 投了
+	Chudan, // 中断
+	Sennichite, // 千日手
+	TimeUp, // 手番側が時間切れで負け
+	IllegalMove, // 手番側の反則負け、反則の内容はコメントで記録する
+	SIllegalAction, // 先手(下手)の反則行為により、後手(上手)の勝ち
+	GIllegalAction, // 後手(上手)の反則行為により、先手(下手)の勝ち
+	Jishogi, // 持将棋
+	Kachi, // (入玉で)勝ちの宣言
+	Hikiwake, // (入玉で)引き分けの宣言
+	Matta, // 待った
+	Tsumi, // 詰み
+	Fuzumi, // 不詰
+	Error, // エラー
+}
+impl TryFrom<String,TypeConvertError<String>> for EndState {
+	fn try_from(kind:String) -> Result<EndState,TypeConvertError<String>> {
+		Ok(match &*kind {
+			"%TORYO" => EndState::Toryo,
+			"%CHUDAN" => EndState::Chudan,
+			"%SENNICHITE" => EndState::Sennichite,
+			"%TIME_UP" => EndState::TimeUp,
+			"%ILLEGAL_MOVE" => EndState::IllegalMove,
+			"%+ILLEGAL_ACTION" => EndState::SIllegalAction,
+			"%-ILLEGAL_ACTION" => EndState::GIllegalAction,
+			"%JISHOGI" => EndState::Jishogi,
+			"%KACHI" => EndState::Kachi,
+			"%HIKIWAKE" => EndState::Hikiwake,
+			"%MATTA" => EndState::Matta,
+			"%TSUMI" => EndState::Tsumi,
+			"%FUZUMI" => EndState::Fuzumi,
+			"%ERROR" => EndState::Error,
+			_ => {
+				return Err(TypeConvertError::SyntaxError(String::from("Invalid end state.")));
+			}
+		})
 	}
 }
